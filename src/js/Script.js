99fab5cadb986788cc8d2970878f37bf691b7ed9
dezir15 <90769415+dezir15@users.noreply.github.com>
2022-02-24 16:12:49 +0600

    Add files via upload

--- src/js/Script.js
+++ src/js/Script.js
@@ -1,47 +1,46 @@
-"use strict"
-
 let siteCost = 1000;
-let type = prompt(`Стоимость сайта: ${siteCost} руб.\nВведите тип сайта: `);
-console.log("Ваш тип сайта: " + type);
-
-let design = prompt("Укажите дизайн сайта(по умолчанию: Обычный): \n	1. Современный\t\t\t+200 руб.\n	2. Ретро\t\t\t\t\t+150 руб.\n	3. Постапокалипсис\t\t+300 руб.\n	4. Игровой\t\t\t\t+250 руб.");
-let yourDesign;
 let designCost = 0;
-if(design == 1){
-	yourDesign = "Современный"; 
-	designCost = 200;
-}
-else if(design == 2){
-	yourDesign = "Ретро";
-	designCost = 150;
-}
-else if(design == 3){
-	yourDesign = "Постапокалипсис";
-	designCost = 300;
-}
-else if(design == 4){
-	yourDesign = "Игровой";
-	designCost = 250;
-}
-else{
-	yourDesign = "Обычный";
-}
 
-console.log("Выбранный вами дизайн: " + yourDesign);
+function calculator(){
+	var Design = $('#design').val();
+	var adapt = $('#adapt').val();
+	var siteType = $('#site__type').val();
+	var timing = 0;
 
-let sum = siteCost + designCost;
+	if(siteType == 1){
+		timing = 15;
+	}
+	else if(siteType == 2){
+		timing = 20;
+	}
+	else if(siteType == 3){
+		timing = 25;
+	}
+	else if(siteType == 4){
+		timing = 30;
+	}
 
-let adapt = confirm("Желаете адаптивный сайт(+50% к стоимости)?");
-let yourAdapt;
-if(adapt == true){
-	console.log("Вы выбрали сайт с адаптацией");
-	yourAdapt = "Да";
-	sum += sum * 0.5;
+	if(Design == 1){
+		designCost = 200;
+	}
+	else if(Design == 2){
+		designCost = 150;
+	}
+	else if(Design == 3){
+		designCost = 300;
+	}
+	else if(Design == 4){
+		designCost = 250;
+	}
+
+	let sum = siteCost + designCost;
+
+	let yourAdapt;
+	if(adapt == 1){
+		sum += sum * 0.5;
+		timing += 5;
+	}
+
+	$('#sum').html(sum);
+	$('#timing').html(timing);
 }
-else{
-	console.log("Вы выбрали сайт без адаптации");
-	yourAdapt = "Нет";
-}
-
-
-alert(`Ваш выбор:\n\tТип сайта: ${type}\n\tДизайн: ${yourDesign}\n\tАдаптация: ${yourAdapt} \n\nСумма: ${sum} руб`)
